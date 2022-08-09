a44c878ce329dfe0dca081d346915af9aa708b6d
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-08-09 18:23:13 +0300

    add routes

--- src/component/TodoContainer.js
+++ src/component/TodoContainer.js
@@ -3,7 +3,10 @@ import Header from "./Header"
 import InputTodo from "./Input"
 import TodosList from "./TodoList"
 import { v4 as uuidv4 } from "uuid"
-import { Route, Switch } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import About from '../pages/About'
+import NotMatch from '../pages/NotMatch'
+import Navbar from "./Navbar"
 
 const TodoContainer = () => {
   const [todos, setTodos] = useState([])
@@ -51,20 +54,25 @@ const TodoContainer = () => {
   }
 
   return (
-    <Route path='/'>
-      <div className="container">
-        <div className="inner">
-          <Header />
-          <InputTodo addTodoProps={addTodoItem} />
-          <TodosList
-            todos={todos}
-            handleChangeProps={handleChange}
-            deleteTodoProps={delTodo}
-            setUpdate={setUpdate}
-          />
-        </div>
-      </div>
-    </Route>
+    <>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={(<div className="container">
+            <div className="inner">
+              <Header />
+              <InputTodo addTodoProps={addTodoItem} />
+              <TodosList
+                todos={todos}
+                handleChangeProps={handleChange}
+                deleteTodoProps={delTodo}
+                setUpdate={setUpdate}
+              />
+            </div>
+          </div>)} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotMatch />} />
+      </Routes>
+    </>
   )
 }
 
